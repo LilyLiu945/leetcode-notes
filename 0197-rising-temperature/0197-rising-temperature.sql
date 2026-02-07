b20ec2af756bcfb6473a1d2db66eb39d9107f6dc
LilyLiu945 <zhijiel9@uci.edu>
2026-02-06 17:10:45 -0800

    Time: 394 ms (71.49%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w0
+  ON w0.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w0.temperature;
